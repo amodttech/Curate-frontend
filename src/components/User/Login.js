5c1326fb535f22326442eb711717f78d58285a0d
Aaron <aaron@amodttech.com>
2021-03-04 14:51:18 -0500

    auth implemented

--- src/components/User/Login.js
+++ src/components/User/Login.js
@@ -16,19 +16,31 @@ function Login() {
   // USESTATES
   const [formUsername, setFormUsername] = useState(null)
   const [password, setPassword] = useState(null)
+  const [error, setError] = useState(false)
   // HANDLERS
   function handleSubmit(e) {
     e.preventDefault()
-    const formData = { formUsername, password }
+    const formData = { username: formUsername, password: password }
     fetch("http://localhost:3000/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(formData)
     })
-    .then((r) => r.json())
-    .then((userObj) => {
+    .then(r => r.json())
+    .then(response => {
+      if (response.status === "error"){
+        setError(true)
+      }
+      else{
+        setError(false)
+        userState(response)
+      }
+    })
+  }
+
+  function userState(userObj){
         dispatch(isLoggedIn())
         dispatch(setId(userObj.id))
         dispatch(setDisplayName(userObj.display_name))
@@ -36,7 +48,6 @@ function Login() {
         dispatch(addExhibitions(userObj.exhibitions))
         dispatch(setUsername(userObj.username))
         history.push("/")
-    })
   }
   //// ------------
 
@@ -51,6 +62,7 @@ function Login() {
           <input className="login-input" type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
           <p></p>
           <button type="submit">SUBMIT</button>
+          {error ? <p className="error">user dont exist</p> : null}
       </form>
     </div>
   );
